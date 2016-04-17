3a70a7861c5cf10a89b6dce80f94fc696ab902d7
GX_PC <284026530@qq.com>
2016-04-17 15:40:45 +0800

    修改输出测试报告相对路径

--- dealaction.py
+++ dealaction.py
@@ -16,19 +16,28 @@ class XiaoShu(unittest.TestCase):
         self.FristWinLogin(user="testjoin",pwd="a12345678",method="1")
         time.sleep(2)
         self.Borrow(type="25")
-        self.BorrowStepone(money="5000",repaytime="9",QQ="1",describe="testtesttesttesttesttest",status="提交审核",type="25")
-        time.sleep(10)
-
+        self.BorrowStepone(money="5000",
+                           repaytime="9",
+                           QQ="1",
+                           describe="testtesttesttesttesttest",
+                           status="提交审核"
+                           ,type="25")
+        # time.sleep(10)
+        self.Quitbrow()
     def tearDown(self):
         self.Quitbrow()
         # self.assertEqual([],self.v)
 
-
 if __name__ == "__main__":
-    testunit = unittest.TestSuite()   #定义一个单元测试容器
+    # 定义一个单元测试容器
+    testunit = unittest.TestSuite()
     testunit.addTest(XiaoShu("setUp"))  #将测试用例加入到测试容器中
-    filename = './PycharmProjects/test.html' #定义个报告存放路径，支持相对路径。
-
+    # 取前面时间
+    now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+    # 定义个报告存放路径，支持相对路径。
+    filename = "./TestReport/"+now+'_TestReport.html'
     fp = file(filename,'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'借款申请测试报告',description=u'用例执行情况：')  #使用HTMLTestRunner配置参数，输出报告路径、报告标题、描述
-    runner.run(testunit) #自动进行测试
+    # 使用HTMLTestRunner配置参数，输出报告路径、报告标题、描述
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'借款申请测试报告',description=u'用例执行情况：')
+    # 自动进行测试
+    runner.run(testunit)
